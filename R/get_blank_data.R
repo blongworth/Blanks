9d2146947f71e202ed2d86d517f211792fc7c4c0
Brett Longworth <blongworth@gmail.com>
2019-02-05 15:32:53 -0500

    Combine jme and osg queries using LEFT JOIN.

--- R/get_blank_data.R
+++ R/get_blank_data.R
@@ -22,33 +22,20 @@ getRaw <- function(from = '2014-09-01') {
   db <- amstools::conNOSAMS()
   
   #Get raw blank data
-  raw =  odbc::dbGetQuery(db, paste("
+  raw <- odbc::dbGetQuery(db, paste("
     SELECT runtime, target.tp_date_pressed, target.rec_num, 
       sample_name, target.tp_num, gf_co2_qty, 
       he12c, he13c, d13c, he14_12, he13_12, wheel, ok_calc
-    FROM snics_raw, target, graphite
-    WHERE target.tp_num = snics_raw.tp_num
-    AND target.osg_num = graphite.osg_num
-    AND tp_date_pressed > '", from, "'
-    AND target.rec_num IN (83028, 53804, 2138, 140548, 36168, 55101, 1081, 39246)
+    FROM snics_raw
+    JOIN target ON target.tp_num = snics_raw.tp_num
+    LEFT JOIN graphite
+    ON target.osg_num = graphite.osg_num
+    WHERE tp_date_pressed > '", from, "'
+    AND target.rec_num IN (83028, 53804, 2138, 140548, 36168, 55101, 1081, 39246, 32490, 32491, 32492, 36947, 148820)
     "))
-  
-  jmer =  odbc::dbGetQuery(db, paste("
-    SELECT runtime, target.tp_date_pressed, target.rec_num, 
-      sample_name, target.tp_num, 
-      he12c, he13c, d13c, he14_12, he13_12, wheel, ok_calc
-    FROM snics_raw, target
-    WHERE target.tp_num = snics_raw.tp_num
-    AND tp_date_pressed > '", from, "'
-    AND target.rec_num IN (32490, 32491, 32492, 36947, 148820)
-    "))
-  
-  #add type columns, combine data frames
-  jmer$gf_co2_qty <- NA
-  blanks.r <- rbind(raw, jmer)
   
   #average by target and filter
-  blanks.a <- blanks.r %>%
+  raw %>%
     group_by(tp_num) %>%
     summarize(
       he12c = mean(ifelse(ok_calc == 1, he12c, NA), na.rm = TRUE),
@@ -56,8 +43,6 @@ getRaw <- function(from = '2014-09-01') {
       flagged = ( (n() - sum(ok_calc == 1)) / n()) # fraction of runs flagged
     ) %>%
     mutate(c1412x = he1412 * 1e16) 
-
-blanks.a
 
 }
 
@@ -79,28 +64,15 @@ getNorm <- function(from = '2014-09-01') {
             target.rec_num, target.tp_num, gf_co2_qty, 
             norm_ratio, int_err, ext_err, 
             blk_corr_method, fm_corr, sig_fm_corr, ss
-          FROM snics_results, target, graphite
-          WHERE target.tp_num = snics_results.tp_num
-          AND target.osg_num = graphite.osg_num
-          AND tp_date_pressed > '", from, "'
-          AND target.rec_num IN (83028, 53804, 2138, 140548, 36168, 55101, 1081, 39246)
+          FROM snics_results
+          JOIN target
+          ON snics_results.tp_num = target.tp_num
+          LEFT JOIN graphite
+          ON target.osg_num = graphite.osg_num
+          WHERE tp_date_pressed > '", from, "'
+          AND target.rec_num IN (83028, 53804, 2138, 140548, 36168, 55101, 1081, 39246, 32490, 32491, 32492, 36947, 148820)
           "))
   
-  
-  jme =  dbGetQuery(db, paste("
-         SELECT runtime, wheel, target.tp_date_pressed, sample_name, 
-             target.rec_num, target.tp_num,
-             norm_ratio, int_err, ext_err,
-             blk_corr_method, fm_corr, sig_fm_corr, ss
-           FROM snics_results, target
-           WHERE target.tp_num = snics_results.tp_num
-           AND target.rec_num IN (32490, 32491, 32492, 36947, 148820)
-           AND tp_date_pressed > '", from, "'
-           "))
-  
-  jme$gf_co2_qty <- NA
-  blanks.n <- rbind(blanks.n, jme)
-
   blanks.n
 
 }
